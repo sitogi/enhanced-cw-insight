851a8061023246d9ccc2c01abf7641278a2b8dfc
sitogi <takano162@gmail.com>
2024-12-19 22:06:25 +0900

    起動制御をこまかくした

--- src/content.ts
+++ src/content.ts
@@ -2,4 +2,23 @@ import { linkifyRequestId } from './features/linkifyRequestId';
 
 console.log('enhanced-cw-insight is loaded!');
 
-linkifyRequestId();
+let isInsightOpened = false;
+
+const handleHashChange = () => {
+  if (window.location.hash.includes('#logsV2:logs-insights')) {
+    if (isInsightOpened) {
+      return;
+    }
+    isInsightOpened = true;
+    linkifyRequestId();
+  } else {
+    isInsightOpened = false;
+  }
+};
+
+// 初回読み込み時にも実行する
+handleHashChange();
+
+// CloudWatch のコンソールは SPA で構築しており、ハッシュによってページ分割されているため、
+// hashchange イベントを利用してページ遷移を検知し、ログのインサイトのときのみ動作するようにする
+window.addEventListener('hashchange', handleHashChange);
